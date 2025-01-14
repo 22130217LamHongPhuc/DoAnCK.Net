e454922a2887d5757525c478deb64f90c5a64261
lam hong phuc <22130217@st.hcmuaf.edu.vn>
2025-01-14 09:35:02 +0700

    complete

--- API.Net/API.Net/Controllers/UserController.cs
+++ API.Net/API.Net/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using API.Net.Models;
+using API.Net.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -43,6 +44,87 @@ namespace API.Net.Controllers
             return Ok(user);
 
         }
+
+        [HttpGet("login")]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest(new { message = "Email và mật khẩu không được để trống." });
+            }
+
+            try
+            {
+                
+                var user = await _context.Users
+                    .FirstOrDefaultAsync(u => u.Email == email && u.Password==password);
+
+              if(user == null)
+                {
+                    return StatusCode(404, new { message = "Khong tim thay user." });
+
+                }
+
+                return Ok(user);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Đã xảy ra lỗi trong quá trình xử lý.", error = ex.Message });
+            }
+        }
+
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserModel usermodel)
+        {
+
+
+            User user = new User
+            {
+                FullName = usermodel.FullName,
+                Email = usermodel.Email,
+                PhoneNumber = usermodel.PhoneNumber,
+                Password = usermodel.Password
+            };
+            try
+            {
+                
+
+                user.CreateAt = DateTime.UtcNow;          
+                user.RoleId = 1;                         
+                user.PhoneNumber = user.PhoneNumber ?? 0; 
+
+                // Lưu vào cơ sở dữ liệu
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+
+                return Ok(new
+                {
+                    message = "Đăng ký thành công.",
+                    user = new
+                    {
+                        user.UserId,
+
+                        user.FullName,
+                        user.Email,
+                        user.PhoneNumber,
+            
+                        user.CreateAt
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Đã xảy ra lỗi trong quá trình xử lý.", error = ex.Message });
+            }
+        }
+
+
+
+
+
 
 
     }
